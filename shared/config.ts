c94e9b3f4fa13e51715c79bc00d49a92e897f906
R3DIANCE <r3diance@gmail.com>
2024-05-25 17:23:03 +0300

    Update config.ts

--- shared/config.ts
+++ shared/config.ts
@@ -1,8 +1,8 @@
 export const ROLL_COOLDOWN = 4500;       // Combat rolling spam prevention
-export const relativeHeading = 0.12;       // Weapon upwards ratio when not shooting
-export const relativePitch = 0.12;         // Weapon left and right ratio when not shooting
-export const relativeHeadingNotAiming = 1; //  Weapon upwards ratio when not shooting
-export const relativePitchNotAiming = 1;   // Weapon left and right ratio when not shooting
+export const relativeHeading = 0.12;       // Weapon upwards ratio when aiming
+export const relativePitch = 0.12;         // Weapon left and right ratio when aiming
+export const relativeHeadingNotAiming = 1; //  Weapon upwards ratio when not aiming
+export const relativePitchNotAiming = 1;   // Weapon left and right ratio when not aiming
 export const shootingLevel = 1000;         // Weapon accuracy
 
 export default { ROLL_COOLDOWN, relativeHeading, relativePitch, relativeHeadingNotAiming, relativePitchNotAiming, shootingLevel };
